21d62deb80d0ebf7dffcbbb8c523861d521cb01e
Efe Hascan <serefefehascan@ogrenci.beykoz.edu.tr>
2025-03-23 18:08:43 +0300

    Trigger altyapı & control

--- Assets/QuizGame/Scripts/PlayerController.cs
+++ Assets/QuizGame/Scripts/PlayerController.cs
@@ -1,18 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Singleton
+    
+    public static PlayerController Singleton { get; private set; }
+
+    private void Awake()
     {
-        
+        if (Singleton != null && Singleton != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Singleton = this;
+        DontDestroyOnLoad(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    #endregion
+    
+    
+    [SerializeField] Rigidbody2D rb;
+    
+    private float moveHorizontal = 2f;
+    private float jumpPower = 2f;
+    private float jumpDuration = 0.5f;
+    
+    private bool canMove = true;
+
+    private void Update()
     {
-        
+        Movement();
+    }
+
+
+    private void FreezePlayer()
+    {
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+    }
+
+
+    private void Movement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Vector2 move = new Vector2(horizontalInput * moveHorizontal, rb.velocity.y);
+        rb.velocity = move;
     }
 }
